884a11c8d6e96d689b27bdc66aaa949a2221532e
Tinh Van Le <tinh-van.le@ap.omron.net>
2021-08-19 17:22:34 +0700

    a

---
+++ build_client/ArrayImagesScada.js
@@ -0,0 +1,16 @@
+import CSC_R from './ImagesScada/CSC_R.gif'
+import CSC_S from './ImagesScada/CSC_S.gif'
+import CSC_F from './ImagesScada/CSC_F.gif'
+import CSC_L from './ImagesScada/CSC_L.gif'
+
+export const arrImage  = [
+    {
+        src_R: CSC_R,
+        src_S: CSC_S,
+        src_L: CSC_F,
+        src_F: CSC_L,
+        name: 'Motor2_Up'
+    },
+];
+  
+  export default arrImage
